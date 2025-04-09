642a3b140f95f367b890c5a66f00b5ea482d682f
Satyam Chaubey <satyamchaubey2729@gmail.com>
2025-04-09 17:15:19 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://github.com/stym-29/Klimate">
+          Download Source Code
+        </a>
+        {""}| Developed by{" "}
+        <a target="_blank" href="https://my-portfolio-six-tau-59.vercel.app/">
+          Satyam Chaubey
+        </a>{" "}
+        | Powered by{" "}
+        {/* <a target="_blank" href="https://my-portfolio-six-tau-59.vercel.app/">
+          SyAyLab
+        </a> */}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "d6b0c5d611b8eaef5cae96381677e521",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
